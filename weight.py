448ddf1257bb36489b352d8ee75c56f145e95c0f
VKsaseepan <136046061+VKsaseepan@users.noreply.github.com>
2023-06-24 09:14:47 +0530

    Add files via upload

---
+++ weight.py
@@ -0,0 +1,5 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:20:04) [MSC v.1929 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> weight=49
+>>> print(weight)
+49
